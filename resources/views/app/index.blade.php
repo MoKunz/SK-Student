cebcbfd8c9b8590c9682a033b1ed0b9661d7451b
MoKunz <pavat.tangmo@hotmail.com>
2017-07-03 07:18:08 +0700

    Theme color

--- resources/views/app/index.blade.php
+++ resources/views/app/index.blade.php
@@ -11,7 +11,8 @@
     <link rel="apple-touch-icon" sizes="180x180" href="/img/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="/img/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="/img/favicon-16x16.png">
-    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+    <!-- Chrome, Firefox OS and Opera -->
+    <meta name="theme-color" content="#2196f3">
 @endsection
 
 @section('body')
